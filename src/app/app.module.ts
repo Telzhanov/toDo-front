9d702a65f24fea014123f56c22c90d68cf0a6190
Askhat Telzhanov <askhat@codebusters.team>
2019-04-23 22:35:27 +0600

    lab2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TaskListsComponent } from './task-lists/task-lists.component';
+import { ProviderService } from './shared/services/provider.service';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { TaskListsTasksComponent } from './task-lists-tasks/task-lists-tasks.component';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
+import { TaskListDetailComponent } from './task-list-detail/task-list-detail.component';
+import { BaseComponent } from './base/base.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListsComponent,
+    TaskListsTasksComponent,
+    TaskDetailComponent,
+    TaskListDetailComponent,
+    BaseComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ProviderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/models.ts
@@ -0,0 +1,28 @@
+export interface ITaskList {
+  id: number,
+  name: string;
+}
+
+export interface ITaskShort {
+  id: number,
+  name: string,
+  status: string;
+}
+
+export interface ITaskLong {
+  id: number;
+  name: string;
+  created_at: string;
+  due_on: string;
+  status: string;
+}
+
+export interface ITaskCreated {
+  id: number;
+  name: string;
+  created_at: string;
+  due_on: string;
+  status: string;
+  task_list: ITaskList;
+}
+
